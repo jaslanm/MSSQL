cca96a7c31b1ba431d07579cbe310568c06ffce1
Michalm <jaslanm@gmail.com>
2022-02-21 21:48:28 +0100

    templatka extraktora

--- extractor.py
+++ extractor.py
@@ -1,3 +1,15 @@
-#learning GIT
+import pandas as pd
+import pyodbc 
 
-#asda
+conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server=RON\SQLEXPRESS;'
+                      'Database=test_database;'
+                      'Trusted_Connection=yes;')
+
+sql_query = pd.read_sql_query(''' 
+                              select * from test_database.dbo.product
+                              '''
+                              ,conn) # here, the 'conn' is the variable that contains your database connection information from step 2
+
+df = pd.DataFrame(sql_query)
+df.to_csv (r'C:\Users\Ron\Desktop\exported_data.csv', index = False) # place 'r' before the path name
